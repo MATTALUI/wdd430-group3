c339c3506988440117720c31d3f0a747b4c41365
Matt Hummer <biggreyson@gmail.com>
2024-04-26 17:10:08 -0600

    stub pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex-1">
+    <div>
       Group 3
-    </main>
+    </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-red-950 p-4 text-gray-100">
+      footer
+    </footer>
+  );
+}
