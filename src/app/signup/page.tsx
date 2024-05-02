d1aab3285345bbe0c33ea622378bc0cdb552ff07
Cesar Coutino <ccoutino@gmail.com>
2024-05-02 15:04:28 -0700

    Type of user

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ export default function SignupPage() {
     <div>
       <div className="w-full max-w-xs">
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 className="text-lg font-bold mb-2">Sign Up</h1>
+        <h1 className="text-lg font-bold mb-2">Create Account</h1>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -59,6 +59,27 @@ export default function SignupPage() {
               Repeat Password
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="repeatPassword" type="password" placeholder="********" />
+          </div>
+          <div className="mb-4">
+            <span className="block text-gray-700 text-sm font-bold mb-2">User Type</span>
+            <div className="mb-2">
+              <input
+                type="radio"
+                id="buyer"
+                name="userType"
+                value="buyer"
+              />
+              <label htmlFor="buyer">Buyer</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="seller"
+                name="userType"
+                value="seller"
+              />
+              <label htmlFor="seller">Seller (artisans & crafters)</label>
+            </div>
           </div>
           <div className="flex items-center justify-between">
             <button className="bg-accent hover:opacity-75 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
