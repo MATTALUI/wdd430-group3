5d4da2f36663864855902783c4513795acbcf16a
Cesar Coutino <ccoutino@gmail.com>
2024-05-02 15:23:08 -0700

    Update text size

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -54,22 +54,23 @@ export default function SignupPage() {
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-0 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
           </div>
-          <div className="mb-6">
+          <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="repeatPassword">
               Repeat Password
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="repeatPassword" type="password" placeholder="********" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-0 leading-tight focus:outline-none focus:shadow-outline" id="repeatPassword" type="password" placeholder="********" />
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <span className="block text-gray-700 text-sm font-bold mb-2">User Type</span>
-            <div className="mb-2">
+            <div className="flex items-center mb-2">
               <input
                 type="radio"
                 id="buyer"
                 name="userType"
                 value="buyer"
+                className="appearance-none border rounded-full w-4 h-4 border-gray-400 checked:bg-gray-500 checked:border-transparent focus:outline-none"
               />
-              <label htmlFor="buyer">Buyer</label>
+              <label htmlFor="buyer" className="ml-2" style={{ fontSize: '0.85rem' }}>Buyer (clients)</label>
             </div>
             <div>
               <input
@@ -77,8 +78,9 @@ export default function SignupPage() {
                 id="seller"
                 name="userType"
                 value="seller"
+                className="appearance-none border rounded-full w-4 h-4 border-gray-400 checked:bg-gray-500 checked:border-transparent focus:outline-none"
               />
-              <label htmlFor="seller">Seller (artisans & crafters)</label>
+              <label htmlFor="seller" className="ml-2" style={{ fontSize: '0.85rem' }}>Seller (artisans & crafters)</label>
             </div>
           </div>
           <div className="flex items-center justify-between">
