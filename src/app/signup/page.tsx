1979179442400b8fea1cf56eee4b7dae109d0667
Cesar Coutino <ccoutino@gmail.com>
2024-05-02 22:46:39 -0700

    update margin before button

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -54,7 +54,7 @@ export default function SignupPage() {
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-0 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="repeatPassword">
               Repeat Password
             </label>
