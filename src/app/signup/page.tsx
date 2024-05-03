c27fb0dd50f9e1481e96a0ef39fbbba5952366a7
Cesar Coutino <ccoutino@gmail.com>
2024-05-02 22:40:02 -0700

    Sign up update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,15 +9,15 @@ export default function SignupPage() {
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Name"
+              htmlFor="FirstName"
             >
-              Name
+              First Name
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
+              id="firstName"
               type="text"
-              placeholder="Name"
+              placeholder="First Name"
             />
           </div>
           <div className="mb-4">
@@ -59,29 +59,6 @@ export default function SignupPage() {
               Repeat Password
             </label>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-0 leading-tight focus:outline-none focus:shadow-outline" id="repeatPassword" type="password" placeholder="********" />
-          </div>
-          <div className="mb-6">
-            <span className="block text-gray-700 text-sm font-bold mb-2">User Type</span>
-            <div className="flex items-center mb-2">
-              <input
-                type="radio"
-                id="buyer"
-                name="userType"
-                value="buyer"
-                className="appearance-none border rounded-full w-4 h-4 border-gray-400 checked:bg-gray-500 checked:border-transparent focus:outline-none"
-              />
-              <label htmlFor="buyer" className="ml-2 text-sm">Buyer (clients)</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="seller"
-                name="userType"
-                value="seller"
-                className="appearance-none border rounded-full w-4 h-4 border-gray-400 checked:bg-gray-500 checked:border-transparent focus:outline-none"
-              />
-              <label htmlFor="seller" className="ml-2 text-sm">Seller (artisans & crafters)</label>
-            </div>
           </div>
           <div className="flex items-center justify-between">
             <button className="bg-accent hover:opacity-75 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
