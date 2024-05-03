8aa4e0b2ba3085d0c4db28f6823480b39fe7f55a
Matt Hummer <biggreyson@gmail.com>
2024-05-03 13:43:10 -0600

    doc: improve comments

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -10,7 +10,6 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log("user:", auth?.user || "null");
       const isLoggedIn = !!auth?.user;
       const isInProtectedPath =
         protectedPatterns.some((pattern) => pattern.test(nextUrl.pathname));
@@ -20,35 +19,5 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [], // providers get set in ./auth.ts
 } satisfies NextAuthConfig;
-/*
-providers: [Credentials({
-  authorize: async (credentials) => {
-    console.log("running authorize")
-    try {
-      const parsedCredentials = z
-        .object({ email: z.string().email(), password: z.string().min(6) })
-        .safeParse(credentials);
-
-      if (parsedCredentials.success) {
-        const { email, password } = parsedCredentials.data;
-        const { user, passwordHash } = await getUserAuthentication({ email });
-        console.log({ user, passwordHash });
-        if (!user) return null;
-        // return true;
-        // const passwordsMatch = await bcrypt.compare(password, passwordHash);
-        // console.log({ passwordsMatch });
-
-        // if (passwordsMatch) return user;
-        return null;
-      }
-      console.log("returning normally")
-      return null;
-    } catch (e) {
-      // console.error(e);
-      return null;
-    }
-  },
-}),],
-*/
