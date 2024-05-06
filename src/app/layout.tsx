e3bf8e4adc0db9f8d219d0611a87e2f002ee346c
BelChaparro <a.belenchaparro@gmail.com>
2024-05-06 17:43:17 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-  <SessionProvider>
-    <html lang="en">
-      <body
-        className={clsx(nunito.className, "flex min-h-screen flex-col bg-light-trans")}
-      >
-        <Navbar />
-        <main className="flex-1 p-4">
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={clsx(nunito.className, "flex min-h-screen flex-col bg-light-trans")}
+        >
+          <Navbar />
+          <main className="flex-1 p-4">
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
     </SessionProvider>
   );
 }
