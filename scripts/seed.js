d5681f0726594e1a8e90fb0b5f18236648205e1d
Cesar Coutino <ccoutino@gmail.com>
2024-05-26 09:03:02 -0700

    Update files

--- scripts/seed.js
+++ scripts/seed.js
@@ -8,7 +8,36 @@ const cleanupOldData = async (client) => {
   await client.sql`DROP TABLE IF EXISTS group3_reviews;`;
   await client.sql`DROP TABLE IF EXISTS group3_product_images;`;
   await client.sql`DROP TABLE IF EXISTS group3_products;`;
+  await client.sql`DROP TABLE IF EXISTS group3_categories;`;
   await client.sql`DROP TABLE IF EXISTS group3_users;`;
+}
+
+const seedCategories = async (client) => {
+  console.log("  Seeding Categories");
+
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS group3_categories (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );
+  `;
+
+  const categories = ["Candles", "Jewelry", "Pottery", "Textiles", "Paintings", "Music"];
+
+  const createdCategories = await Promise.all(categories.map(async (name) => {
+    const result = await client.sql`
+      INSERT INTO group3_categories (name)
+      VALUES (${name})
+      RETURNING *;
+    `;
+    const [insertedCategory] = result.rows;
+    console.log(`    - Created category: ${insertedCategory.name} (${insertedCategory.id})`);
+    return insertedCategory;
+  }));
+
+  created.categories = createdCategories;
+
+  return;
 }
 
 const seedUsers = async (client) => {
@@ -73,11 +102,13 @@ const seedProducts = async (client) => {
       CREATE TABLE IF NOT EXISTS group3_products (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         seller_id UUID NOT NULL,
+        category_id UUID,
         name VARCHAR(255) NOT NULL,
         description TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW(),
-        FOREIGN KEY (seller_id) REFERENCES group3_users(id)
+        FOREIGN KEY (seller_id) REFERENCES group3_users(id),
+        FOREIGN KEY (category_id) REFERENCES group3_categories(id)
       );
     `;
   await client.sql`
@@ -96,6 +127,7 @@ const seedProducts = async (client) => {
       name: "Vanilla Dream Candle",
       descripion: "Indulge in the luxurious aroma of Vanilla Dream, where creamy vanilla notes intertwine with whispers of delicate florals, enveloping your space in an enchanting ambiance of elegance and tranquility. Elevate your senses and adorn your surroundings with the essence of opulence, as Vanilla Dream casts a sophisticated spell of warmth and sophistication.",
       sellerIndex: 1,
+      categoryIndex: 0,
       images: [
         "https://i5.walmartimages.com/asr/02111a4f-4152-479e-84e5-6d041784c882_1.8024209556070c8bcf41823a02f017fa.jpeg",
         "https://images.prismic.io/bareblends/4becbce8-cc6e-4292-a25c-93d6136a5df7_vanilla+bean+hero.jpg",
@@ -105,6 +137,7 @@ const seedProducts = async (client) => {
       name: "Lavender Fields Candle",
       descripion: "Transport your senses to the rolling hills of Provence with Lavender Fields, where the essence of freshly bloomed lavender dances gracefully upon a gentle breeze, infusing your sanctuary with an air of refined serenity. Immerse yourself in the epitome of sophistication as Lavender Fields envelops your space, inviting you to luxuriate in moments of pure indulgence and relaxation.",
       sellerIndex: 1,
+      categoryIndex: 0,
       images: [
         "https://www.pagangrimoire.com/wp-content/uploads/2020/05/Purple-Candle-Meaning-1200x800.jpg",
         "https://worldoffloweringplants.com/wp-content/uploads/2017/11/Lavandula-angustifolia-English-Lavender1.jpg",
@@ -114,6 +147,7 @@ const seedProducts = async (client) => {
       name: "Campfire Nights Candle",
       descripion: "Embark on an olfactory journey reminiscent of sophisticated evenings under the stars with Campfire Nights, where smoky notes of crackling birchwood intertwine with hints of spicy cedar, evoking memories of opulent outdoor escapades. Embrace the allure of refined rusticity as Campfire Nights casts a luxurious glow, inviting you to bask in the warmth of its elegant ambiance.",
       sellerIndex: 1,
+      categoryIndex: 0,
       images: [
         "https://i.etsystatic.com/26990167/r/il/0426e3/3055778669/il_fullxfull.3055778669_mstg.jpg",
       ],
@@ -122,6 +156,7 @@ const seedProducts = async (client) => {
       name: "Cherry Blossom Candle",
       descripion: "Immerse yourself in the exquisite fragrance of Cherry Blossom, where each delicate note unveils a symphony of floral opulence, transforming any space into a sanctuary of refined tranquility and sophistication. Let the essence of Cherry Blossom elevate your surroundings with its subtle yet captivating allure, inviting you to luxuriate in moments of sheer indulgence and understated elegance.",
       sellerIndex: 1,
+      categoryIndex: 0,
       images: [
         "https://assets.myntassets.com/h_200,w_200,c_fill,g_auto/h_1440,q_100,w_1080/v1/assets/images/7736144/2018/11/3/a2cb9b3f-b76d-491c-a1bb-42ed3d42d5211541240984131-Archies-Pink-Scented-Candle-7011541240983930-1.jpg",
         "https://www.godsavethepoints.com/wp-content/uploads/2018/09/57911622_m.jpg",
@@ -131,15 +166,17 @@ const seedProducts = async (client) => {
       name: "Hunter's Track",
       descripion: "A homemade album of a former secret lover",
       sellerIndex: 1,
+      categoryIndex: 5,
       images: [],
     },
   ];
 
   const createdProducts = await Promise.all(products.map(async (product) => {
     const sellerId = created.users[product.sellerIndex].id;
+    const categoryId = created.categories[product.categoryIndex].id;
     const result = await client.sql`
-      INSERT INTO group3_products (name, description, seller_id)
-      VALUES (${product.name}, ${product.descripion}, ${sellerId})
+      INSERT INTO group3_products (name, description, seller_id, category_id)
+      VALUES (${product.name}, ${product.descripion}, ${sellerId}, ${categoryId})
       RETURNING *;
     `;
     const [insertedProduct] = result.rows;
@@ -197,7 +234,7 @@ const seedReviews = async (client) => {
       RETURNING *;
     `;
   }));
-  console.log(`    - Created ${createdReviews.length} reviews`);
+  console.log(`    - Created  ${createdReviews.length} reviews`);
   created.reviews = createdReviews;
 }
 
@@ -206,7 +243,9 @@ const main = async () => {
   console.log("Seeding Database");
   try {
     await cleanupOldData(client);
+    await seedCategories(client);
     await seedUsers(client);
+    
     await seedProducts(client);
     await seedReviews(client);
   } finally {
