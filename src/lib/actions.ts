3cc92a0c32925ef960ee28f623c1764eeae1b227
BelChaparro <a.belenchaparro@gmail.com>
2024-05-28 05:39:47 -0300

    added createProduct action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,11 @@
 "use server"
 
 import { signIn } from '@/auth';
+import { sql } from '@vercel/postgres';
 import { AuthError } from 'next-auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from "zod";
  
 export async function authenticate(
   prevState: string | undefined,
@@ -20,4 +24,48 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+  // Validate data types with zod:
+
+const ImagesSchema = z.object({
+  id: z.string(),
+  product_id: z.string(),
+  src: z.string(),
+})
+const FormSchema = z.object({
+  id: z.string(),
+  seller_id: z.string(),
+  name: z.string(),
+  price: z.coerce
+      .number()
+      .gt(0, { message: "The price must be greater than $0." }),
+  description: z.string(),
+  images: z.array(ImagesSchema)
+});
+
+const CreateProduct = FormSchema.omit( { id: true, images: true });
+
+
+export async function createProduct(formData: FormData) {
+  
+  const { seller_id, name, price, description } = CreateProduct.parse({
+    seller_id: formData.get("seller_id"),
+    name: formData.get("name"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  });
+  const priceInCents = price * 100;
+
+  //Insert data into the DB:
+  try {
+    await sql`
+      INSERT INTO group3_products (seller_id, name, price, description)
+      VALUES (${seller_id}, ${name}, ${priceInCents}, ${description});
+    `;
+  } catch(error) {
+    return { message: "Database Error: Failed to Create New Product" }
+  }
+    revalidatePath('/products/'); // Clear cache and trigger new request to the server
+    redirect('/products'); // Redirect the user back to the /products page
 }
