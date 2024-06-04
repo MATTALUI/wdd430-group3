54995db6b7a26ad4e13db0902429ed910c607c3f
Matt Hummer <biggreyson@gmail.com>
2024-06-03 21:57:19 -0700

    🔑

--- src/components/CategoriesDashSkeleton.tsx
+++ src/components/CategoriesDashSkeleton.tsx
@@ -10,7 +10,7 @@ export default async function CategoriesDashSkeleton() {
             <h2 className="text-xl font-bold my-5 text-center">Categories</h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-5  justify-items-center">
                 {mockCategories.map((_, i) => (
-                    <div className="w-full">
+                    <div key={i} className="w-full">
                         <div className="animate-pulse opacity-25 bg-white h-60 rounded-full"></div>
                     </div>
                 ))}
