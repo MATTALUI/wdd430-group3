207a7e00ebb184e10d658ee696234799056f6386
Matt Hummer <biggreyson@gmail.com>
2024-06-03 21:38:29 -0700

    move dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import { getProducts, getUser } from "@/lib/data";
+import TopProducts from "@/components/popularItems";
+import CategoriesMenu from "@/components/categoriesMenu";
+import { Suspense } from "react";
+import PopularItemsSkeletons from "@/components/PopularItemsSkeletons";
 
-export default async function Home() {
+export default async function Dashboard() {
   return (
     <div>
-      Group 3
+        <div
+            className="sm:mx-40 mx-auto w-auto bg-cover bg-bottom"
+            style={{
+                height: '300px',
+                backgroundImage: 'url(https://images.pexels.com/photos/3654776/pexels-photo-3654776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+            }}
+        ></div>
+        <Suspense fallback={<PopularItemsSkeletons />}>
+            <TopProducts />
+        </Suspense>
+
+        <Suspense>
+            <CategoriesMenu />
+        </Suspense>
+
     </div>
   );
 }
