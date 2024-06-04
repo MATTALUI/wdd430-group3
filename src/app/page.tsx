8a29330bd3b5582c3502f57c5ad27df912a80054
Matt Hummer <biggreyson@gmail.com>
2024-06-03 21:53:39 -0700

    skeletons 💀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,26 @@ import TopProducts from "@/components/popularItems";
 import CategoriesMenu from "@/components/categoriesMenu";
 import { Suspense } from "react";
 import PopularItemsSkeletons from "@/components/PopularItemsSkeletons";
+import CategoriesDashSkeleton from "@/components/CategoriesDashSkeleton";
 
 export default async function Dashboard() {
   return (
     <div>
-        <div
-            className="sm:mx-40 mx-auto w-auto bg-cover bg-bottom"
-            style={{
-                height: '300px',
-                backgroundImage: 'url(https://images.pexels.com/photos/3654776/pexels-photo-3654776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
-            }}
-        ></div>
-        <Suspense fallback={<PopularItemsSkeletons />}>
-            <TopProducts />
-        </Suspense>
+      <div
+        className="sm:mx-40 mx-auto w-auto bg-cover bg-bottom"
+        style={{
+          height: '300px',
+          backgroundImage: 'url(https://images.pexels.com/photos/3654776/pexels-photo-3654776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+        }}
+      ></div>
+      <Suspense fallback={<PopularItemsSkeletons />}>
+        <TopProducts />
+      </Suspense>
 
-        <Suspense>
-            <CategoriesMenu />
-        </Suspense>
+
+      <Suspense fallback={<CategoriesDashSkeleton />}>
+        <CategoriesMenu />
+      </Suspense>
 
     </div>
   );
